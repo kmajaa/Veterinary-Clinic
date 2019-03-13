5086428d03dc8aea59362f30f576cc7cc5d5c142
Michał Domino <michdom215@student.polsl.pl>
2019-03-13 19:34:01 +0100

    first commit

---
+++ src/main/java/pl/polsl/veterynaryclinic/VeterynaryclinicApplication.java
@@ -0,0 +1,14 @@
+package pl.polsl.veterynaryclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterynaryclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterynaryclinicApplication.class, args);
+		a
+	}
+
+}
